69262c84cfd1ca417c7e1cd951909a4e988e5cd4
OmiiiDev <103533638+OmiiiDev@users.noreply.github.com>
2023-12-07 15:12:03 -0500

    Delete UnlimitedCookie.js

--- UnlimitedCookie.js
+++
@@ -1,5 +0,0 @@
-// Get the cookie
-var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)example_cookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-
-// Set the cookie with an extended expiration date
-document.cookie = "example_cookie=" + cookieValue + "; expires=" + new Date(new Date().getTime() + (30 * 24 * 60 * 60 * 1000)).toUTCString() + "; path=/";
